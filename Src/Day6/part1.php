484144dababf37558a5ede34667f778602ae7133
Gabriel Trosell <gabriel.trosell@gmail.com>
2022-12-06 11:32:00 +0100

    day 6 part 2 working

--- Src/Day6/part1.php
+++ Src/Day6/part1.php
@@ -9,20 +9,34 @@ class Part1
     public function __construct()
     {
         $input = file(__DIR__ . '/input.txt');
-
+        $question1 = 0;
+        $question2 = 0;
         foreach ($input as $row => $signal) {
-            echo 'Checking signal on row ' . $row . "\n";
+            // echo 'Checking signal on row ' . $row . "\n";
+            $signal = mb_strtolower(trim($signal));
             $signalLenght = (strlen($signal) - 5);
 
+            $startOfPacketLenght = 4;
             for ($i = 0; $i < $signalLenght; $i++) {
-                $startOfPacket = substr($signal, $i, 4);
-                $unique = str_split(mb_strtolower(trim($startOfPacket)));
-                if (count(array_unique($unique)) == 4) {
-                    echo 'First Marker of 4 different unique characters: ' . $startOfPacket . '  ends ' . $i + 4 . " chars in\n";
+                $startOfPacket = substr($signal, $i, $startOfPacketLenght);
+                if (count(array_unique(str_split($startOfPacket))) == $startOfPacketLenght) {
+                    $question1 = $i + $startOfPacketLenght;
+                    // echo 'Found 4 charachter long Start Of Packet: \'' . $startOfPacket . '\' and it ends ' . $question1 . " chars in\n";
 
                     break;
                 }
             }
+
+            $startOfMessageLenght = 14;
+            for ($i = 0; $i < $signalLenght; $i++) {
+                $startOfMessage = substr($signal, $i, $startOfMessageLenght);
+                if (count(array_unique(str_split($startOfMessage))) == $startOfMessageLenght) {
+                    $question2 = $i + $startOfMessageLenght;
+                    // echo 'Found 14 charachter long Start Of Message: \'' . $startOfMessage . '\' and it ends ' . $question2. " chars in\n";
+                    break;
+                }
+            }
         }
+        echo 'Answer Day 6 - Question 1: ' . $question1 . ' Question 2: ' . $question2;
     }
 }
