a176204a1b6407cbc157ef81d8cdbb643d6e72cf
Gabriel Trosell <gabriel.trosell@gmail.com>
2022-12-06 11:58:13 +0100

    refactor

--- Src/Day6/part1.php
+++ Src/Day6/part1.php
@@ -9,13 +9,7 @@ class Part1
     public function __construct()
     {
         $input = file(__DIR__ . '/input.txt');
-        $question1 = 0;
-        $question2 = 0;
-        foreach ($input as $row => $signal) {
-            $question1 = $this->getSequenceEnd($signal, 4);
-            $question2 = $this->getSequenceEnd($signal, 14);
-        }
-        echo 'Answer Day 6 - Question 1: ' . $question1 . ' Question 2: ' . $question2;
+        echo 'Answer Day 6 - Question 1: ' . $this->getSequenceEnd($input[0], 4) . ' Question 2: ' . $this->getSequenceEnd($input[0], 14);
     }
 
     public function getSequenceEnd(string $SignalCheck, int $length)
