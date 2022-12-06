c5e449f21981ebf0e9a8486645fe0e737b1042f4
Gabriel Trosell <gabriel.trosell@gmail.com>
2022-12-06 11:53:59 +0100

    refactor

--- Src/Day6/part1.php
+++ Src/Day6/part1.php
@@ -12,31 +12,19 @@ class Part1
         $question1 = 0;
         $question2 = 0;
         foreach ($input as $row => $signal) {
-            // echo 'Checking signal on row ' . $row . "\n";
-            $signal = mb_strtolower(trim($signal));
-            $signalLenght = (strlen($signal) - 5);
-
-            $startOfPacketLenght = 4;
-            for ($i = 0; $i < $signalLenght; $i++) {
-                $startOfPacket = substr($signal, $i, $startOfPacketLenght);
-                if (count(array_unique(str_split($startOfPacket))) == $startOfPacketLenght) {
-                    $question1 = $i + $startOfPacketLenght;
-                    // echo 'Found 4 charachter long Start Of Packet: \'' . $startOfPacket . '\' and it ends ' . $question1 . " chars in\n";
-
-                    break;
-                }
-            }
-
-            $startOfMessageLenght = 14;
-            for ($i = 0; $i < $signalLenght; $i++) {
-                $startOfMessage = substr($signal, $i, $startOfMessageLenght);
-                if (count(array_unique(str_split($startOfMessage))) == $startOfMessageLenght) {
-                    $question2 = $i + $startOfMessageLenght;
-                    // echo 'Found 14 charachter long Start Of Message: \'' . $startOfMessage . '\' and it ends ' . $question2. " chars in\n";
-                    break;
-                }
-            }
+            $question1 = $this->getSequenceEnd($signal, 4);
+            $question2 = $this->getSequenceEnd($signal, 14);
         }
         echo 'Answer Day 6 - Question 1: ' . $question1 . ' Question 2: ' . $question2;
+    }
+
+    public function getSequenceEnd(string $SignalCheck, int $length)
+    {
+        $signalLenght = (strlen(mb_strtolower(trim($SignalCheck))) - 5);
+        for ($i = 0; $i <  $signalLenght; $i++) {
+            if (count(array_unique(str_split(substr($SignalCheck, $i, $length)))) == $length) {
+                return ($i + $length);
+            }
+        }
     }
 }
