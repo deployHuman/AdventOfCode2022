32ffe8f3a56927c0a263eb1d54053db8a98898b9
Gabriel Trosell <gabriel.trosell@gmail.com>
2022-12-07 17:48:10 +0100

    adding arg to run specific day or all of them

--- index.php
+++ index.php
@@ -53,4 +53,23 @@ function createFolder(string $fullpath)
     }
 }
 
-require_once $fullPathTotodaysFile;
+$allDays = scandir($homepath . '/');
+unset($allDays[0]);
+unset($allDays[1]);
+
+if (empty($argv[1])) {
+    require_once $fullPathTotodaysFile;
+} else {
+    if ($argv[1] == 'all') {
+        foreach ($allDays as $key => $value) {
+            require_once $homepath . '/' . $value . '/' . $value . '.php';
+        }
+    } elseif (! empty($argv) && is_numeric($argv[1])) {
+        $specificday = $homepath . '/Day' . $argv[1] . '/Day' . $argv[1] . '.php';
+        if (file_exists($specificday)) {
+            require_once $specificday;
+        } else {
+            echo "That day doesn't exist";
+        }
+    }
+}
